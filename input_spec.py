Removes 'beta_' prefix for various streamlit commands. Fixes bug in limiting magnitude calculation. Improves aperture figure function.

input_spec.py
@@ -19,12 +19,12 @@ h = 6.626068E-27    # cm^2*g/s
 k = 1.3806503E-16   # cm^2*g/(s^2*K)
 Ang = 1E-8          # cm
 mu = 1E-4           # cm
-page_cont = st.beta_container()
+page_cont = st.container()
 
 
-col1,col2 = page_cont.beta_columns(2)
-page_container = st.beta_container()
-with page_container.beta_container():
+col1,col2 = page_cont.columns(2)
+page_container = st.container()
+with page_container.container():
     num1 = col1.number_input('First Number:',min_value=0.1, value=1.5, max_value=5.)
     num2 = col2.number_input('Second Number:', min_value = 1000., value = 2000., max_value=5000.)
 
@@ -147,9 +147,9 @@ def exec_gui(page_container=None, side_container=None):
     psfdir = config.get('CONFIG', 'psfdir')
 
     #   Page title, set 2 columns for page format. Setup the configuration sidebar.
-    container = page_container.beta_container()
+    container = page_container.container()
     container.title('OSIRIS Exposure Time Calculator')
-    col1, col2 = container.beta_columns(2)
+    col1, col2 = container.columns(2)
     col1.subheader('OSIRIS Configuration')
 
     #   select the mode for the ETC -- Imager or IFS
@@ -305,11 +305,11 @@ def exec_gui(page_container=None, side_container=None):
         col1.write('Calculating Integration Time for Input SNR')
 
     #   select flux type and input the source flux
-    side_cont = side_container.beta_container()
+    side_cont = side_container.container()
     side_cont.subheader('Source Properties')
     fl = side_cont.selectbox('Input Flux Method:', ['Magnitude', 'Flux Density', 'Integrated Flux over Bandpass'])
     if fl == 'Magnitude':
-        side_col1, side_col2 = side_cont.beta_columns([3, 1])
+        side_col1, side_col2 = side_cont.columns([3, 1])
         mag = side_col1.number_input('Magnitude: ', value=20.)
         veg = side_col2.radio('Magnitude Standard:', ('Vega', 'AB'))
         if veg == 'AB':
@@ -391,7 +391,7 @@ def exec_gui(page_container=None, side_container=None):
         line_width = 200.
         lam_obs = lmean * 1e-3
     if spec == 'Phoenix Stellar Library Spectrum':
-        pcol1, pcol2 = side_container.beta_columns(2)
+        pcol1, pcol2 = side_container.columns(2)
         teff = pcol1.selectbox('Teff (K):',
                                [str(int(i)) for i in np.append(np.arange(23, 70) * 100, np.arange(35, 61) * 200)])
         logg = pcol1.selectbox('log(g):', [str(i)[0:3] for i in np.arange(0, 13) / 2.])
@@ -446,7 +446,7 @@ def exec_gui(page_container=None, side_container=None):
         wave = np.linspace(lmin / 1e3, lmax / 1e3, dxspectrum)
         specinput = [np.array(wave), np.zeros((dxspectrum))]
         page_container.subheader('Input Spectrum Generator')
-        specol1, specol2, specol3 = page_container.beta_columns(3)
+        specol1, specol2, specol3 = page_container.columns(3)
         l = 1
         k = 1
         spec1 = specol1.selectbox('Add Spectrum:', ['None', 'Vega', 'Flat', 'Emission',
@@ -714,8 +714,8 @@ def exec_gui(page_container=None, side_container=None):
         specinput = None
 
     ##input PSF configuration
-    with page_container.beta_container():
-        psf_col1, psf_col2 = page_container.beta_columns(2)
+    with page_container.container():
+        psf_col1, psf_col2 = page_container.columns(2)
         psf_col1.subheader('Point Spread Function (PSF) Configuration:')
         psfmode = psf_col1.selectbox('Select PSF Option:', ['Generated Analytic PSF', 'Pre-generated SCAO PSF'])
         if psfmode == 'Pre-generated SCAO PSF':
@@ -766,8 +766,8 @@ def exec_gui(page_container=None, side_container=None):
         radiusl = 40 * lmean * 1e2 * 206265 / (1.26e11)
 
     #   Container for Results and result plots.
-    with page_container.beta_container():
-        col3, col4 = page_container.beta_columns(2)
+    with page_container.container():
+        col3, col4 = page_container.columns(2)
         col3.subheader('OSIRIS Calculation Results:')
         #   Calculation
         aperture = col3.slider(label='Aperture Radius (arcseconds):', min_value=0.01,

liger_snr_sim.py
@@ -271,7 +271,7 @@ def LIGER_ETC(filter = "K", mag = 21.0, flambda=1.62e-19, fint=4e-17, itime = 1.
         flux_phot = 1.
     else:
         print('No Flux values provided. Returning.')
-        return '', '', ''
+        return '', '', '', ''
     #########################################################################
     #########################################################################
     # comparison:
@@ -367,7 +367,27 @@ def LIGER_ETC(filter = "K", mag = 21.0, flambda=1.62e-19, fint=4e-17, itime = 1.
     #Second Aperture lambda dependent
     aperturel = CircularAperture([xs,ys], r=radiusl)
     maskl = aperturel.to_mask(method='exact')
-
+    # The overall way this code works is to define string values which
+    # are then returned in the ordered dictionary, and the string values
+    # that aren't empty are shown as the results of the simulator code.
+    # So here we define all the string values and then fill in the ones for
+    # each mode depending on what the calculation is.
+    peakSNR = ""
+    medianSNR = ""
+    meanSNR = ""
+    medianSNRl = ""
+    meanSNRl = ""
+    totalSNRl = ""
+    peakmag = ""
+    minexptime = ""
+    medianexptimel = ""
+    meanexptimel = ""
+    totalexptimel = ""  # integrated aperture exptime at pre-defined fixed aperture
+    peakFlux = ""
+    totalFlux = ""
+    totalFlux_aper = ""
+    fluxmag = ""
+    fluxmag_aper = ""
     ###########################################################################
     ###########################################################################
     # IFS MODE
@@ -525,22 +545,6 @@ def LIGER_ETC(filter = "K", mag = 21.0, flambda=1.62e-19, fint=4e-17, itime = 1.
             sat_pix = ''
             saturated = ''
         totalObservedCube = totalObservedCube.astype(np.float64)
-        peakSNR = ""
-        medianSNR = ""
-        meanSNR = ""
-        medianSNRl = ""
-        meanSNRl = ""
-        totalSNRl = ""
-        peakmag = ""
-        minexptime = ""
-        medianexptimel = ""
-        meanexptimel = ""
-        totalexptimel = ""  # integrated aperture exptime at pre-defined fixed aperture
-        peakFlux = ""
-        totalFlux = ""
-        totalFlux_aper = ""
-        fluxmag = ""
-        fluxmag_aper = ""
         ####################################################
         # Case 1: find s/n for a given exposure time and mag
         ####################################################
@@ -816,50 +820,46 @@ def LIGER_ETC(filter = "K", mag = 21.0, flambda=1.62e-19, fint=4e-17, itime = 1.
             #subimage = subimage/np.sum(subimage)
             cubesize = np.shape(observedCube)
             onesimg = np.ones((cubesize[1], cubesize[2]))
-            cube = observedCube * np.sqrt(itime * nframes) / np.sqrt(observedCube + noisetotal)
-            #cube = ((subimage) * spec_temp[:, np.newaxis, np.newaxis]).astype(np.float32)
             if mag_calc == "peak":
+                noiseval = (noisetotal[(np.where(observedCube == np.max(observedCube))[0])[0]])
+                sigval = np.max(observedCube)
+                factor = (sigval * snr ** 2. + sigval * snr * np.sqrt(4. * noiseval * itime * nframes + snr ** 2.)) / (
+                            2. * sigval ** 2. * itime * nframes)
+
+                cube = observedCube * factor * np.sqrt(itime * nframes) / np.sqrt(observedCube * factor + noisetotal)
                 snr_cube = snr * cube / (np.max(cube))
                 noisetotal = noisetotal
             elif mag_calc == "total":
                 intsum = np.array([])
+                intback = np.array([])
                 for i in range(len(wave)):
-                    intsum = np.append(intsum, np.sum(cube[i,:,:]))
+                    intback = np.append(intback, np.sum(noisetotal[i, :, :]))
+                    intsum = np.append(intsum, np.sum(observedCube[i, :, :]))
+                intback = np.trapz(intback, x=wave)
                 intcube = np.trapz(intsum, x=wave)
-                snr_cube = snr * cube/intcube
+                noiseval = intback
+                sigval = intcube
+                factor = (sigval * snr ** 2. + sigval * snr * np.sqrt(4. * noiseval * itime * nframes + snr ** 2.)) / (
+                        2. * sigval ** 2. * itime * nframes)
+                cube = observedCube * factor * np.sqrt(itime * nframes) / np.sqrt(observedCube * factor + noisetotal)
+                intsum = np.array([])
+                for i in range(len(wave)):
+                    intsum = np.append(intsum, np.sum(cube[i, :, :]))
+                intcube = np.trapz(intsum, x=wave)
+                snr_cube = snr * cube / intcube
                 noisetotal = noisetotal
                 print('intcube:', intcube)
             elif mag_calc == "per_wav":
+                cube = observedCube * np.sqrt(itime * nframes) / np.sqrt(observedCube + noisetotal)
                 snr_cube = snr * cube
                 noisetotal = noisetotal
-
-            print('collecting area:', collarea)
-            print('scale:', scale)
-            print('efftot:', efftot)
-            print('dark current:', darknoise)
-            print('readnoise :', readnoise)
-            print('noisefactor:', noise_factor)
-            print('resolution:', resolution)
-            print('snr:', snr)
-            print('intflux:', intFlux)
-            print('int noise:', intnoise)
-            print('peak cube:', np.max(cube))
-            print('peak snr cube:' , np.max(snr_cube))
             fluxcube = (snr_cube ** 2. + np.sqrt(
-                snr_cube ** 4. + 4. * snr_cube ** 2. * noisetotal * itime * nframes)) / (2. * itime * nframes)
-            fluxcube /= (efftot * collarea * subimgcube)
+                snr_cube ** 4. + 4. * (snr_cube ** 2.) * noisetotal * itime * nframes)) / (2. * itime * nframes)
+            fluxcube /= (efftot * collarea * (wave[1] - wave[0]))
             totFlux_spec = np.array([])
-            peakspec = fluxcube[:, xs, ys]
-            peakint = np.trapz(peakspec, x=wave)
-            peakmag = vega2ab(-2.5 * np.log10(float(peakint*(1./np.max(subimage))) / zp), lambdac / 10.)
             for i in range(dxspectrum):
                 totFlux_spec = np.append(totFlux_spec, np.sum(fluxcube[i, :, :]))
-            totFlux = np.trapz(totFlux_spec, x=wave) * (1./np.sum(subimage))
-            print('lost flux factor: ', (1./np.sum(subimage)))
-            print('PSF maximum: ', np.max(subimage))
-            print('PSF sum: ', np.sum(subimage))
-            print('Total Integrated vega Magnitude:', -2.5 * np.log10(float(totFlux) / zp) )
-            print('Total Integrated AB Magnitude:', vega2ab(-2.5 * np.log10(float(totFlux) / zp), lambdac / 10.))
+            totFlux = np.trapz(totFlux_spec, x=wave) * (1. / np.sum(subimage))
 
             #### This code computes the aperture calculation
             #### signal flux required to yield input SNR for the flux within specified aperture
@@ -938,9 +938,9 @@ def LIGER_ETC(filter = "K", mag = 21.0, flambda=1.62e-19, fint=4e-17, itime = 1.
                 totalFlux_aper = str(
                     "%0.2f*$10^{-17} ergs/s/cm^2$" % (totFlux_aper * np.mean(E_ph) * (1. / (100 * 100)) * 1e17))
             else:
-                peakFlux = str("%0.1f" %np.max(fluxcube))
-                totalFlux = str("%0.2f" % totFlux)  # str("%0.2f"%totFlux)
-                totalFlux_aper = str("%0.2f" % totFlux_aper)
+                peakFlux = str("%0.1f $photons/s/m^{2}$" %np.max(fluxcube))
+                totalFlux = str("%0.2f $photons/s/m^{2}$" % totFlux)  # str("%0.2f"%totFlux)
+                totalFlux_aper = str("%0.2f $photons/s/m^{2}$" % totFlux_aper)
             fluxmag = str("%0.2f" % vega2ab(-2.5 * np.log10(float(totFlux) / zp), lambdac / 10.))
             fluxmag_aper = str("%0.2f" % vega2ab(-2.5 * np.log10(float(totFlux_aper) / zp), lambdac / 10.))
             # peakFlux
@@ -1271,7 +1271,7 @@ def LIGER_ETC(filter = "K", mag = 21.0, flambda=1.62e-19, fint=4e-17, itime = 1.
                             ('Median Value of SNR (Aperture Radius = ' + "{:.3f}".format(sizel/2.) + '")', medianSNRl),
                             ('Mean Value of SNR (Aperture Radius = ' + "{:.3f}".format(sizel/2.) + '")', meanSNRl),
                             ('Peak Value of required flux for input SNR', peakFlux),
-                            ('Integrated Total Limiting Flux for input SNR(For flux within Aperture Radius = ' + "{:.3f}".format(
+                            ('Integrated Total Limiting Flux for input SNR (For flux within Aperture Radius = ' + "{:.3f}".format(
                                 sizel / 2.) + '")', totalFlux_aper),
                             ('Integrated Total Limiting AB Magnitude for input SNR (For flux within Aperture Radius = ' + "{:.3f}".format(
                                 sizel / 2.) + '")', fluxmag_aper),

pages/gui_main.py
@@ -109,9 +109,9 @@ def exec_gui(page_container=None, side_container=None):
     psfdir = config.get('CONFIG', 'psfdir')
 
     #   Page title, set 2 columns for page format. Setup the configuration sidebar.
-    container = page_container.beta_container()
+    container = page_container.container()
     container.subheader('Instrument Configuration')
-    col1, col2 = container.beta_columns(2)
+    col1, col2 = container.columns(2)
 
     scale = col1.number_input('Plate Scale ( Milircseconds per Spatial Pixel )', value=14.)
     if 'fov' not in st.session_state:
@@ -273,11 +273,11 @@ def exec_gui(page_container=None, side_container=None):
         col1.write('Calculating Source Flux Required for input Integration Time and Input SNR' + calc_title)
     #st.session_state.snr = snr
     #   select flux type and input the source flux
-    side_cont = side_container.beta_container()
+    side_cont = side_container.container()
     side_cont.subheader('Source Properties')
     if calc != 'Limiting Flux':
         fl = side_cont.selectbox('Input Flux Method:', ['Magnitude', 'Flux Density','Integrated Flux over Bandpass'])
-        side_col1, side_col2 = side_cont.beta_columns([3, 1])
+        side_col1, side_col2 = side_cont.columns([3, 1])
         if fl == 'Magnitude':
             mag = side_col1.number_input('Magnitude: ', value = 20.)
             veg = side_col2.radio('Magnitude Standard:', ('Vega', 'AB'))
@@ -346,7 +346,7 @@ def exec_gui(page_container=None, side_container=None):
 
     if source == 'Extended (Mag/Sq. Arcsec.)':
         if prof_type == 'Sersic Profile':
-            prof_side_col1, prof_side_col2 = side_container.beta_columns([2,3])
+            prof_side_col1, prof_side_col2 = side_container.columns([2,3])
             from misc_funcs import sersic_profile
             s_ind = prof_side_col1.selectbox('Sersic Index:', [1,2,3,4])
             r_eff = prof_side_col2.number_input('Effective Radius ("):', min_value=etc_scale, value=0.5)
@@ -390,7 +390,7 @@ def exec_gui(page_container=None, side_container=None):
 #   Deal with input spectrum
     if spec == 'Emission':
         lam_obs = side_container.number_input('Line Wavelength (nm)', min_value=lmin, value=lmean, max_value=lmax)
-        eside_col1, eside_col2 = side_container.beta_columns([3, 1])
+        eside_col1, eside_col2 = side_container.columns([3, 1])
         emis_unit = eside_col2.radio('Width Unit:',['km/s', 'nm'])
         if emis_unit == 'km/s':
             line_width = eside_col1.number_input('Line Width (km/s):', min_value=1., value=200.)
@@ -402,7 +402,7 @@ def exec_gui(page_container=None, side_container=None):
         line_width = 200.
         lam_obs = lmean*1e-3
     if spec == 'Phoenix Stellar Library Spectrum':
-        pcol1, pcol2 = side_container.beta_columns(2)
+        pcol1, pcol2 = side_container.columns(2)
         teff = pcol1.selectbox('Teff (K):', [str(int(i)) for i in np.append(np.arange(23,70)*100, np.arange(35,61)*200)])
         logg = pcol1.selectbox('log(g):', [str(i)[0:3] for i in np.arange(0,13)/2.])
         feh = pcol2.selectbox('Fe/H:', ['-4.0', '-3.0', '-2.0', '-1.5', '-1.0', '-0.5', '-0.0', '+0.5', '+1.0'], index=6)
@@ -437,7 +437,7 @@ def exec_gui(page_container=None, side_container=None):
         spec_temp = flux_phot * spec_temp / np.trapz(spec_temp, x=wave)
         specinput = [np.array(wave), np.array(spec_temp)]
     elif spec == 'Stellar Population Spectra - Maraston & Stromback (2011)':
-        pcol1, pcol2 = side_container.beta_columns(2)
+        pcol1, pcol2 = side_container.columns(2)
         speclib = pcol1.selectbox('Stellar Population Library:', ['MILES', 'STELIB', 'ELODIE',
                                                                     'MARCS', 'LRG'])
         if speclib == 'LRG':
@@ -540,7 +540,7 @@ def exec_gui(page_container=None, side_container=None):
     elif spec == 'Custom':
         specinput = [np.array(wave), np.zeros((dxspectrum))]
         page_container.subheader('Input Spectrum Generator')
-        specol1, specol2, specol3, specol4 = page_container.beta_columns(4)
+        specol1, specol2, specol3, specol4 = page_container.columns(4)
 
         def add_spec(spec1, k, filt='K', wave=None, scale=None, fint=None, flambda=None, mag=None, source=None,
              simdir='~/Liger/sim', lam_obs=None, line_width=None, mode='ifs'):
@@ -903,10 +903,10 @@ def exec_gui(page_container=None, side_container=None):
     else:
         radiusl = 40 * lmean * 1e2 * 206265 / (1.26e11)
 
-    with page_container.beta_container():
+    with page_container.container():
 
         ##input PSF configuration
-        psf_col1, psf_col2 = st.beta_columns(2)
+        psf_col1, psf_col2 = st.columns(2)
         psf_col1.subheader('Point Spread Function (PSF) Configuration:')
         psfmode = psf_col1.selectbox('Select PSF Option:', ['Generated Analytic PSF','Pre-generated LTAO PSF'])
         if psfmode == 'Pre-generated LTAO PSF':
@@ -949,7 +949,7 @@ def exec_gui(page_container=None, side_container=None):
             #     blur = True
             psf_col2.markdown('[KAPA Strehl Calculator](http://bhs.astro.berkeley.edu/cgi-bin/kapa_strehl)')
         with st.form(key='calc_form'):
-            col3, col4 = st.beta_columns(2)
+            col3, col4 = st.columns(2)
             col3.subheader('Liger Calculation Results:')
             #   Calculation
             two_lod = 2000. * np.mean([lmin * 10., lmax * 10.]) * 206265. / (1.26e11)